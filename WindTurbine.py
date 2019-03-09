b34c5997841b0d10d074cc835e79b7e2589eed4d
Matthew Teelucksingh <teelucksingh.matthew1@gmail.com>
2019-03-09 16:35:40 -0500

    Update WindTurbine.py
    -Condensed the class Wind.
    -Added the self.clean() and self.compile() functions.

--- WindTurbine.py
+++ WindTurbine.py
@@ -3,7 +3,7 @@ import math as m
 import pandas as pd
 
 
-# Creating a class to store the necessary variables as well as the functions used to calculate wind power and wre-write
+# Creating a class to store the necessary variables as well as the functions used to calculate wind power and re-write
 # the .csv file.
 class Wind (object):
     # Create a function that stores the following variables: the air density, the blade area, the wind speed
@@ -26,11 +26,34 @@ class Wind (object):
         power_eval_mw = round(power_eval_w / 1000000, 4)
         return power_eval_mw
 
+    # Creating the function that extracts the data frame from a .csv and cleans said data frame to include only the
+    # the relevant data:
+    def clean(self, csv):
+        self.csv = csv
+        # Creates a data frame by importing data from an external .csv file:
+        df = pd.read_csv(csv)
+        # Creating a new data frame that contains only the necessary columns: ["Day"],["Month"],["Year"],["WindSpeed"]
+        df_clean = df.drop(["Sec", "WindDir_deg", "WindDirSTD_deg", "1-secGust_m/s", "3-secGust_m/s",
+                            "AtmPr_mb", "AirTemp_°C", "Humidity_%"], axis=1)
+        df_clean = df_clean.drop([0], axis=0)
+        # declaring the variable self.df_clean for use in the next function:
+        self.df_clean = df_clean
+        return df_clean
+
+    # Creating a function that consumes the int num_rows which row in the data frame the function will draw variables
+    # from. The function then uses the values in the selected column as input for the self.calc() function to calculate
+    # power output of a wind turbine. (It is assumed that the selected column contains wind speed variables:
+    def compile(self, num_rows):
+        self.num_rows = num_rows
+        # declaring a data frame as our previous data frame used in the self.clean() function:
+        df = self.df_clean
+        # Applies the self.calc() function to the variable pulled from the data frame:
+        # the values in the .csv are stored as strings so they need to be converted into a floating point decimal:
+        return self.calc(float(df.iloc[0,self.num_rows]))
+
 
 Frame = Wind(1.23, 52, 0.4)
-print(Frame.calc(12))
-
-
-
+print(Frame.clean("Test_Wind_Data.csv").head())
+print(Frame.compile(5))
 
 
