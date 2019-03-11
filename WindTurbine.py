bef5b12b5c7d9e429e71c519c1d112ede6d02549
Matthew Teelucksingh <teelucksingh.matthew1@gmail.com>
2019-03-10 20:10:37 -0400

    Update WindTurbine.py

--- WindTurbine.py
+++ WindTurbine.py
@@ -1,6 +1,7 @@
 # Importing packages:
 import math as m
 import pandas as pd
+import numpy as np
 
 
 # Creating a class to store the necessary variables as well as the functions used to calculate wind power and re-write
@@ -36,6 +37,8 @@ class Wind (object):
         df_clean = df.drop(["Sec", "WindDir_deg", "WindDirSTD_deg", "1-secGust_m/s", "3-secGust_m/s",
                             "AtmPr_mb", "AirTemp_°C", "Humidity_%"], axis=1)
         df_clean = df_clean.drop([0], axis=0)
+        # searching the data frame for all values that contain the error values of "#NUM!" and removing them:
+        df_clean = df_clean[df_clean.iloc[:, 5] != "#NUM!"]
         # declaring the variable self.df_clean for use in the next function:
         self.df_clean = df_clean
         return df_clean
@@ -43,17 +46,31 @@ class Wind (object):
     # Creating a function that consumes the int num_rows which row in the data frame the function will draw variables
     # from. The function then uses the values in the selected column as input for the self.calc() function to calculate
     # power output of a wind turbine. (It is assumed that the selected column contains wind speed variables):
-    def compile(self, num_rows):
-        self.num_rows = num_rows
+    def compile(self):
         # declaring a data frame as our previous data frame used in the self.clean() function:
         df = self.df_clean
-        # Applies the self.calc() function to the variable pulled from the data frame:
-        # the values in the .csv are stored as strings so they need to be converted into a floating point decimal:
-        return self.calc(float(df.iloc[0,self.num_rows]))
+        # Converting the elements of the data frame into the appropriate variable type:
+        df["Day"] = df["Day"].astype(int)
+        df["Month"] = df["Month"].astype(int)
+        df["Year"] = df["Year"].astype(int)
+        df["Hour"] = df["Hour"].astype(int)
+        df["Min"] = df["Min"].astype(int)
+        df["WindSpd_m/s"] = df["WindSpd_m/s"].astype(float)
+        # Creating a separate column that contains the calculated wind power values based on the the [WindSpd_m/s]:
+        df_append = (self.calc(df["WindSpd_m/s"]))
+        # Adding this new column to the existing data frame df:
+        df["Power_Output/MWt"] = df_append.values
+        self.csv = df
+        return df
+
+    # Creating a simple function that writes the now cleaned and compiled data frame to a .csv:
+    def output(self, f_name):
+        df = self.csv
+        self.f_name = f_name
+        df.to_csv(encoding='utf-8', index=False)
 
 
 Frame = Wind(1.23, 52, 0.4)
-print(Frame.clean("Test_Wind_Data.csv").head())
-print(Frame.compile(5))
-
+(Frame.clean("Test_Wind_Data.csv"))
+print(Frame.compile())
 
